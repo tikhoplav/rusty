4d5c545fa33d3fe28bbb0716f5e9efd6d483d784
Vitaly Tikhoplav <tikhoplavspb@gmail.com>
2022-12-15 17:25:10 +0200

    index using module wrapper

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,4 @@
-import Rusty, { loadModule } from "./rusty";
-
-// require("../target/wasm32-unknown-unknown/release/rusty_client.wasm")
-//   .then((wasm: Module) => {
-//     const rusty = new Rusty(wasm);
-
-//     const decoder = new TextDecoder('utf-8');
-
-//     const greetPtr = (wasm.greet as Function)();
-//     const greet = decoder.decode(
-//       new Uint8Array(...rusty.vecBuffer(greetPtr))
-//     );
-//     console.log(greetPtr, greet);
-//     (wasm.destroy as Function)(greetPtr);
-
-//     const motdPtr = (wasm.motd as Function)();
-//     const motd = decoder.decode(
-//       new Uint8Array(...rusty.vecBuffer(motdPtr))
-//     );
-//     console.log(motdPtr, motd);
-//     (wasm.destroy as Function)(motdPtr);
-
-//     const genPtr = (wasm.gen as Function)();
-//     const gen = new Uint32Array(...rusty.vecBuffer(genPtr));
-//     console.log(gen);
-//   })
-
-// const rusty = require("../target/wasm32-unknown-unknown/release/rusty_client.wasm");
-// 
+import { loadModule } from "./rusty";
 
 loadModule("rusty.wasm")
   .then(rusty => {    
